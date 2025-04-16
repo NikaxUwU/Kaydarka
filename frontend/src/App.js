cdd906cd2ae1fb03832ab5fbc5cb24e89276801c
gbudylko <gbudylko@mallenom.ru>
2025-04-16 20:44:11 +0300

    Добавлен реакт и первая верстка

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <html>
+    <header>  
+
+    </header>
+    <body>
+      <div id="main-menu-button">  
+      </div>
+      <div id="main-menu"> 
+      </div>
+    </body>
+    
+    </html>
+  );
+}
+
+export default App;
